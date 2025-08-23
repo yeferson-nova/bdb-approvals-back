b951b3cf8f1783ecf4a64dd0fccaf8fb669873f4
Yeferson Nova <yeferson_nova@outlook.com>
2025-08-22 20:55:07 -0500

    first commit

---
+++ src/main/java/co/com/bancodebogota/BdbApprovalsBackApplication.java
@@ -0,0 +1,13 @@
+package co.com.bancodebogota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdbApprovalsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdbApprovalsBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/bancodebogota/BdbApprovalsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.bancodebogota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdbApprovalsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
